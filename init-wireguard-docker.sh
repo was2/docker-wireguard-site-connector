7cecf2c5a83f6042ce38cbbc2fb050f4fa65a7d0
drew <atlassian@was2.net>
2023-05-31 15:44:00 -0500

    fix docker run line

--- init-wireguard-docker.sh
+++ init-wireguard-docker.sh
@@ -15,7 +15,7 @@ docker build -t wireguard-site-connector .
 docker run -d --name wireguard-site-connector \
     --restart unless-stopped \
     --network host \
-    --cap-add=NET_ADMIN --cap-admin=SYS_ADMIN \
+    --cap-add=NET_ADMIN --cap-add=SYS_ADMIN --privileged \
     -v ${SECRET_STORAGE}:/etc/wireguard-secrets \
     -e WG_IF_ADDRESS="$WG_IF_ADDRESS" \
     -e WG_PEER_ENDPOINT_ADDRESS="$WG_PEER_ENDPOINT_ADDRESS" \
